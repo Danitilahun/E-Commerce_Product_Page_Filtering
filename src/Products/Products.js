efd504aaf788b05c3974ce93161cbb60dedb7977
Daniel Tilahun <tiledan2015@gmail.com>
2024-01-21 21:16:14 +0300

    feat: Create Products component
    - Implemented Products component to render a section of product cards
    - Styled the component using Product.css
    - Utilized the 'result' prop to dynamically generate product cards

--- src/Products/Products.js
+++ src/Products/Products.js
@@ -1,5 +1,15 @@
-import "./Product.css";
+/*
+  Products Component
 
+  This component renders a section containing product cards. 
+  The product cards are dynamically generated based on the 'result' prop.
+  Each product card is expected to be passed as children to this component.
+
+  Example usage:
+  <Products result={/* Array of product cards 
+*/
+
+import "./Product.css";
 const Products = ({ result }) => {
   return (
     <>
